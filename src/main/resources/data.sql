9941b1e0ec525e34f92b0f218e61024b3b6a5373
rmitula <rmitula@gmail.com>
2018-03-14 21:30:50 +0100

    init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO role (role_id, role) VALUES (1, 'ADMIN');
+INSERT INTO role (role_id, role) VALUES (2, 'USER');
+
+
+INSERT INTO user (user_id, email, password, name, last_name, active) VALUES (1, 'user@user', '$2a$10$Anb7jpgtB7VNkiHyV0kvWuYgHsHwKRQMPzQ.EOZLGXgnj/8bSzfha', 'user', 'user', 1);
+INSERT INTO user (user_id, email, password, name, last_name, active) VALUES (2, 'usedr@usder', '$2a$10$Anb7jpgtB7VNkiHyV0kvWuYgHsHwKRQMPzQ.EOZLGXgnj/8bSzfha', 'user', 'user', 1);
+INSERT INTO user (user_id, email, password, name, last_name, active) VALUES (3, 'admin@admin', '$2a$10$Anb7jpgtB7VNkiHyV0kvWuYgHsHwKRQMPzQ.EOZLGXgnj/8bSzfha', 'admin', 'admin', 1);
+
+INSERT INTO user_role (user_id, role_id) VALUES (1, 2);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (3, 2);
