c4a6160f58aa2fc48194dfae116c64eaac48ddae
Bar-Maz-PRz <37315072+Bar-Maz-PRz@users.noreply.github.com>
2018-05-07 19:34:11 +0200

    repo fix

--- src/main/java/pl/edu/prz/kia/universityproject/repository/SpecializationRepository.java
+++ src/main/java/pl/edu/prz/kia/universityproject/repository/SpecializationRepository.java
@@ -2,7 +2,7 @@ package pl.edu.prz.kia.universityproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pl.edu.prz.kia.universityproject.model.Question;
+import pl.edu.prz.kia.universityproject.model.Specialization;
 
 @Repository
 public interface SpecializationRepository extends JpaRepository<Specialization, Long> {
--- src/main/java/pl/edu/prz/kia/universityproject/repository/UserAnswerRepository.java
+++ src/main/java/pl/edu/prz/kia/universityproject/repository/UserAnswerRepository.java
@@ -2,7 +2,7 @@ package pl.edu.prz.kia.universityproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pl.edu.prz.kia.universityproject.model.Question;
+import pl.edu.prz.kia.universityproject.model.UserAnswer;
 
 @Repository
 public interface UserAnswerRepository extends JpaRepository<UserAnswer, Long> {
