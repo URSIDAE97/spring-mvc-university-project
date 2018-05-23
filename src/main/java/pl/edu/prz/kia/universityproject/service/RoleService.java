e8fd79ec92614d2ea381e9251f5e2b3036986878
kmazurek96 <38131290+kmazurek96@users.noreply.github.com>
2018-05-23 23:00:45 +0200

    Add files via upload

--- src/main/java/pl/edu/prz/kia/universityproject/service/RoleService.java
+++ src/main/java/pl/edu/prz/kia/universityproject/service/RoleService.java
@@ -2,7 +2,10 @@ package pl.edu.prz.kia.universityproject.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.edu.prz.kia.universityproject.model.Role;
 import pl.edu.prz.kia.universityproject.repository.RoleRepository;
+
+import java.util.List;
 
 @Service
 public class RoleService {
@@ -13,6 +16,7 @@ public class RoleService {
     public RoleService(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
     }
-    public List<Role> findAll() { return roleRepository.findAll();
-                                }
+
+    public List<Role> findAll() {return roleRepository.findAll();
+    }
 }
